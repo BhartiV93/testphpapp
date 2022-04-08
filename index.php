c5c0f0d1575947843c96335ebab9c6fc4c402983
BhartiV93 <87116878+BhartiV93@users.noreply.github.com>
2022-04-08 18:30:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
-<?php
-
-echo "Hello World!";
-
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Web example</title>
+    </head>
+    <body bgcolor="#FFCCE5">
+        <center>
+        <h1>Welcome</h1>
+        <h2>To</h2>
+        <h3>Tech Saksham</h3>
+        <h4>Cloud</h4>
+        <h5>Computing</h5>
+        <h6>web design</h6>
+        </center>
+        <p>
+            This is <i>my</i> <b>first</b><u>Paragraph</u>    
+            <br><br>
+             I am in the Cloud Computing course
+            <br>
+            Log<sub>10</sub>
+            <br>
+            10<sup>th</sup>
+        </p>
+    </body>
+</html>
